f4364cec340b169a80842f5d9f58422d25e97d61
Michael Lakav <39078877+michaellakav@users.noreply.github.com>
2022-01-21 12:07:18 -0800

    Add files via upload
    updated URL for IP list

--- azure_get_ips.py
+++ azure_get_ips.py
@@ -4,7 +4,7 @@ import json
 import socket
 import sys
 sys.stdout = open('/var/www/html/azure-ips.html', 'w')
-ip_ranges = requests.get('https://download.microsoft.com/download/7/1/D/71D86715-5596-4529-9B13-DA13A5DE5B63/ServiceTags_Public_20211213.json').json()
+ip_ranges = requests.get('https://download.microsoft.com/download/7/1/D/71D86715-5596-4529-9B13-DA13A5DE5B63/ServiceTags_Public_20220117.json').json()
 my_dict = ip_ranges['values']
 
 for item in my_dict:
